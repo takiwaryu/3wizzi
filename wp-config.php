232e8b18605a408995a8fce19ac422a0fb19f438
youssefsaoubou <youssef.saoubou@gmail.com>
2016-04-11 00:27:39 +0100

    db conf

--- wp-config.php
+++ wp-config.php
@@ -16,16 +16,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'gaming');
+define('DB_NAME', 'news');
 
 /** MySQL database username */
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'ABSEC@b369');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', '46.105.15.63');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
